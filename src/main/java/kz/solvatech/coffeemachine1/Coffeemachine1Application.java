1c117ff92c196ddfca996afec6aeb86515f7a54b
SHAMIL <deshomcor@gmail.com>
2025-07-22 12:46:24 +0500

    initial commit

---
+++ src/main/java/kz/solvatech/coffeemachine1/Coffeemachine1Application.java
@@ -0,0 +1,13 @@
+package kz.solvatech.coffeemachine1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coffeemachine1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coffeemachine1Application.class, args);
+    }
+
+}
---
+++ src/test/java/kz/solvatech/coffeemachine1/Coffeemachine1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.solvatech.coffeemachine1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coffeemachine1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
